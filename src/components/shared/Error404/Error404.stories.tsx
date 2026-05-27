06bae4d1e7be8cd5b4107d543b3cae5fc8dcb215
wikt0r3k (Wiktor Osial) <wikt0r3k.kontakt@gmail.com>
2026-05-27 03:12:21 +0200

    feat: Added Error404 component (#17)
    * feat/Error 404 component
    
    * feat: create Error404 component
    
    * reverted package.json and yarn.lock to their original states
    
    * chore: sync with athena updates
    
    * fix: modified heading and bear illustration tests
    
    * changed the order of mocks
    
    * chore: athena update
    
    * minor fix: moved mocks under imports
    
    * fix: changed Button to be consistent with athena, replaced hex with token value and adjusted the mobile view gap to match the desktop view gap
    
    * fix: made the bear img bigger and increased the font size for desktop and changed the Mobile storybook view
    
    * .
    
    * fix: increased button's font size and made it bold

---
+++ src/components/shared/Error404/Error404.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { MemoryRouter } from "react-router";
+import { INITIAL_VIEWPORTS } from "storybook/viewport";
+
+import { Error404 } from "./Error404";
+
+const meta: Meta<typeof Error404> = {
+  title: "Component/Error404",
+  component: Error404,
+  decorators: [
+    (Story) => (
+      <MemoryRouter>
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
+  parameters: {
+    viewport: {
+      options: INITIAL_VIEWPORTS,
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Desktop: Story = {};
+export const Mobile: Story = {
+  globals: {
+    viewport: {
+      value: "iphone6",
+    },
+  },
+};
