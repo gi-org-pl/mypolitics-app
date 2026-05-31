612eb1c8c1c03f89446daa3ee781a9db574fea38
Lukaszglaz <glazzlukasz@gmail.com>
2026-05-31 19:52:28 +0200

    chore(header): update header types

--- src/components/shared/Header/Header.types.ts
+++ src/components/shared/Header/Header.types.ts
@@ -1,6 +1,8 @@
+import type { MessageDescriptor } from "@lingui/core";
+
 export type HeaderNavItem = {
-  key: "debates" | "polls" | "quizzes";
-  label: "Debaty" | "Sondaże" | "Quizy";
+  key: string;
+  label: MessageDescriptor;
   path: string;
   external?: boolean;
 };
