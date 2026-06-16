5f935041780dce054aea1d53b87b7d3d9ced7f5c
Karolina Czarnecka <karolinaczarnecka1671@gmail.com>
2026-06-16 13:03:17 +0200

    Edit after review, checbox, imput

--- src/components/survey/SurveyNewsletter/SurveyNewsletter.tsx
+++ src/components/survey/SurveyNewsletter/SurveyNewsletter.tsx
@@ -11,11 +11,11 @@ export default function SurveyNewsletter({
 }: SurveyNewsletterProps) {
   return (
     <div className="flex flex-col justify-center ">
-      <div className="flex flex-col w-[340px] h-fit bg-gi-dark-ash text-gi-primary rounded-2xl p-4 gap-2.5">
-        <p className="font-bold text-[24px] text-gi-primary w-[308px] h-9 flex items-center">
+      <div className="flex flex-col w-full h-fit text-gi-primary rounded-2xl p-4 gap-2.5">
+        <p className="font-bold text-[24px] text-gi-primary w-full h-9 flex items-center">
           <Trans id="Newsletter.Title" message="Zobacz więcej niż wyniki." />
         </p>
-        <p className="font-normal text-gi-primary w-[308px] h-[72px] flex items-center">
+        <p className="font-normal text-gi-primary w-full h-[72px] items-center">
           <Trans
             id="Newsletter.Description"
             message="Poznaj poglądy innych, porównaj się, zdobądź wiedzę o zmianach w społeczeństwie i Twoim otoczeniu!"
@@ -24,19 +24,18 @@ export default function SurveyNewsletter({
       </div>
       <Input
         type="email"
-        className="w-[340px] h-11 mt-4 mb-4 p-4 rounded-2xl text-gi-primary"
+        className="w-full mt-4 mb-4 rounded-2xl text-gi-primary"
         value={email}
         onChange={onEmailChange}
         placeholder="twoj@mail.com"
       />
-      <div className="flex flex-row w-[340px] h-fit gap-2">
+      <div className="flex flex-row w-full h-fit gap-2">
         <Checkbox
           label=""
-          className="w-3.5 h-3.5 border border-gi-dark-ash rounded-xs cursor-pointer"
           checked={consent}
           onCheckedChange={onConsentChange}
         />
-        <div className="text-[14px] leading-[120%] tracking-normal w-[318px] block text-slate-800">
+        <div className="text-[14px] leading-[120%] tracking-normal w-full block text-slate-800">
           <Trans
             id="Newsletter.Consent"
             message="Wyrażam zgodę na przetwarzanie moich danych osobowych w celu przesyłania mi treści marketingowych przez Fundację Generacja Innowacja."
